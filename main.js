33e02d3a52c91c6ccd4c3466da2cd042a811167e
noelfoka <nomgnenoel@gmail.com>
2023-07-03 13:31:31 +0100

    update linters

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let library = [];
 
 function getInput() {
   const book = {};
-  book.title = document.getElementById("bookTitle").value;
-  book.author = document.getElementById("bookAuthor").value;
+  book.title = document.getElementById('bookTitle').value;
+  book.author = document.getElementById('bookAuthor').value;
   return book;
 }
 
@@ -11,31 +11,31 @@ function removeBook(title) {
   const book = document.getElementById(title);
   book.remove();
   library = library.filter((bookObj) => bookObj.title !== title);
-  localStorage.setItem("library", JSON.stringify(library));
+  localStorage.setItem('library', JSON.stringify(library));
 }
 
 function addBook(bookObj) {
-  const bookList = document.getElementById("book-list");
-  const book = document.createElement("LI");
-  book.setAttribute("id", bookObj.title);
+  const bookList = document.getElementById('book-list');
+  const book = document.createElement('LI');
+  book.setAttribute('id', bookObj.title);
   book.innerHTML = `<h3> ${bookObj.title} </h3> <p>${bookObj.author} </p>`;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerHTML = "Delete";
-  deleteBtn.addEventListener("click", () => removeBook(bookObj.title));
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = 'Delete';
+  deleteBtn.addEventListener('click', () => removeBook(bookObj.title));
   book.appendChild(deleteBtn);
   bookList.appendChild(book);
 }
 
-const addButton = document.getElementById("add-btn");
-addButton.addEventListener("click", () => {
+const addButton = document.getElementById('add-btn');
+addButton.addEventListener('click', () => {
   const book = getInput();
   library.push(book);
-  localStorage.setItem("library", JSON.stringify(library));
+  localStorage.setItem('library', JSON.stringify(library));
   addBook(book);
 });
 
 window.onload = () => {
-  library = JSON.parse(localStorage.getItem("library" || "[]"));
+  library = JSON.parse(localStorage.getItem('library' || '[]'));
   if (library === null) {
     library = [];
     return;
