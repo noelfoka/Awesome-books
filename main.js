647079cbc482a22b1019748b5786c3ad812b9f09
mershark <mersharktakyi@gmail.com>
2023-07-03 13:31:07 +0000

    Modified main.js to not pass data when field is empty

--- main.js
+++ main.js
@@ -1,46 +1,56 @@
 let library = [];
 
-function getInput() {
+const getInput = () => {
   const book = {};
-  book.title = document.getElementById('bookTitle').value;
-  book.author = document.getElementById('bookAuthor').value;
-  return book;
-}
+  const titleInput = document.getElementById('bookTitle');
+  const authorInput = document.getElementById('bookAuthor');
 
-function removeBook(title) {
+  // Return null if either title or author input is empty
+  if (titleInput.value.trim() === '' || authorInput.value.trim() === '') {
+    return null;
+  }
+
+  book.title = titleInput.value;
+  book.author = authorInput.value;
+  return book;
+};
+
+const removeBook = (title) => {
   const book = document.getElementById(title);
   book.remove();
   library = library.filter((bookObj) => bookObj.title !== title);
   localStorage.setItem('library', JSON.stringify(library));
-}
+};
 
-function addBook(bookObj) {
+const addBook = (bookObj) => {
   const bookList = document.getElementById('book-list');
   const book = document.createElement('LI');
   book.setAttribute('id', bookObj.title);
-  book.innerHTML = `<h3> ${bookObj.title} </h3> <p>${bookObj.author} </p>`;
+  book.innerHTML = `<h3>${bookObj.title}</h3><p>${bookObj.author}</p>`;
   const deleteBtn = document.createElement('button');
-  deleteBtn.innerHTML = 'Delete';
+  deleteBtn.innerHTML = 'Remove';
   deleteBtn.addEventListener('click', () => removeBook(bookObj.title));
   book.appendChild(deleteBtn);
   bookList.appendChild(book);
-}
+};
 
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', () => {
   const book = getInput();
-  library.push(book);
-  localStorage.setItem('library', JSON.stringify(library));
-  addBook(book);
-});
 
-window.onload = () => {
-  library = JSON.parse(localStorage.getItem('library' || '[]'));
-  if (library === null) {
-    library = [];
+  // If book is null, do not proceed
+  if (book === null) {
     return;
   }
 
+  library.push(book);
+  addBook(book);
+  localStorage.setItem('library', JSON.stringify(library));
+});
+
+window.onload = () => {
+  library = JSON.parse(localStorage.getItem('library')) || [];
+  
   library.forEach((book) => {
     addBook(book);
   });
