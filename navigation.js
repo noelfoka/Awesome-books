1fb79fd6351fbb3d043c0307f26612c80b4eb8ce
noelfoka <nomgnenoel@gmail.com>
2023-07-06 01:38:02 +0100

    update

--- navigation.js
+++ navigation.js
@@ -1,50 +1,43 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-use-before-define */
 // eslint-disable-next-line no-unused-vars
-function displaySection(sectionId) {
-  const sections = ['list', 'form', 'contact'];
-
-  for (const section of sections) {
-    const element = document.getElementById(section);
-    if (element) {
-      if (section === sectionId) {
-        element.classList.remove('hidden');
-      } else {
-        element.classList.add('hidden');
-      }
-    }
-  }
-}
+function displaySection(section) {
+  const sectionList = document.getElementById("list");
+  const sectionForm = document.getElementById("form");
+  const sectionContact = document.getElementById("contact");
+  const heading = document.getElementById("title");
+  const date = document.getElementById("date");
+  date.textContent = setDate();
 
   // console.log(sectionList, sectionForm, sectionContact);
 
-  /*switch (section) {
-    case 'list':
-      sectionList.classList.remove('hidden');
-      sectionForm.classList.add('hidden');
-      sectionContact.classList.add('hidden');
-      heading.innerHTML = 'All Awesome Books';
+  switch (section) {
+    case "list":
+      sectionList.classList.remove("hidden");
+      sectionForm.classList.add("hidden");
+      sectionContact.classList.add("hidden");
+      heading.innerHTML = "All Awesome Books";
       break;
 
-    case 'form':
-      sectionList.classList.add('hidden');
-      sectionForm.classList.remove('hidden');
-      sectionContact.classList.add('hidden');
-      heading.innerHTML = 'Add a New Book';
+    case "form":
+      sectionList.classList.add("hidden");
+      sectionForm.classList.remove("hidden");
+      sectionContact.classList.add("hidden");
+      heading.innerHTML = "Add a New Book";
       break;
 
-    case 'contact':
-      sectionList.classList.add('hidden');
-      sectionForm.classList.add('hidden');
-      sectionContact.classList.remove('hidden');
-      heading.innerHTML = 'Contact Information';
+    case "contact":
+      sectionList.classList.add("hidden");
+      sectionForm.classList.add("hidden");
+      sectionContact.classList.remove("hidden");
+      heading.innerHTML = "Contact Information";
       break;
 
     default:
       break;
   }
 }
-*/
+
 function setDate() {
   const today = new Date();
   return today.toDateString();
