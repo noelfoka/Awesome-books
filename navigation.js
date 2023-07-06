ccb48a6534a4ba344b11d890caf7437e2c09aa51
noelfoka <nomgnenoel@gmail.com>
2023-07-06 01:43:48 +0100

    fix eslint

--- navigation.js
+++ navigation.js
@@ -2,35 +2,35 @@
 /* eslint-disable no-use-before-define */
 // eslint-disable-next-line no-unused-vars
 function displaySection(section) {
-  const sectionList = document.getElementById("list");
-  const sectionForm = document.getElementById("form");
-  const sectionContact = document.getElementById("contact");
-  const heading = document.getElementById("title");
-  const date = document.getElementById("date");
+  const sectionList = document.getElementById('list');
+  const sectionForm = document.getElementById('form');
+  const sectionContact = document.getElementById('contact');
+  const heading = document.getElementById('title');
+  const date = document.getElementById('date');
   date.textContent = setDate();
 
   // console.log(sectionList, sectionForm, sectionContact);
 
   switch (section) {
-    case "list":
-      sectionList.classList.remove("hidden");
-      sectionForm.classList.add("hidden");
-      sectionContact.classList.add("hidden");
-      heading.innerHTML = "All Awesome Books";
+    case 'list':
+      sectionList.classList.remove('hidden');
+      sectionForm.classList.add('hidden');
+      sectionContact.classList.add('hidden');
+      heading.innerHTML = 'All Awesome Books';
       break;
 
-    case "form":
-      sectionList.classList.add("hidden");
-      sectionForm.classList.remove("hidden");
-      sectionContact.classList.add("hidden");
-      heading.innerHTML = "Add a New Book";
+    case 'form':
+      sectionList.classList.add('hidden');
+      sectionForm.classList.remove('hidden');
+      sectionContact.classList.add('hidden');
+      heading.innerHTML = 'Add a New Book';
       break;
 
-    case "contact":
-      sectionList.classList.add("hidden");
-      sectionForm.classList.add("hidden");
-      sectionContact.classList.remove("hidden");
-      heading.innerHTML = "Contact Information";
+    case 'contact':
+      sectionList.classList.add('hidden');
+      sectionForm.classList.add('hidden');
+      sectionContact.classList.remove('hidden');
+      heading.innerHTML = 'Contact Information';
       break;
 
     default:
